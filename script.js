71e5bd5de41dbc182d937d1aabde6bb2dfe315f9
kmn-kh <kmnpro459@gmail.com>
2024-06-17 15:43:45 +0700

    view pageXOffset and pageYOffset

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 var text = document.getElementById("text");
-var height = document.getElementById("height");
-window.onresize = function () {
-    text.innerHTML = "innerWidth : " + window.innerWidth + "<br>";
-    text.innerHTML += "innerHeight : " +window.innerHeight+"<br>";
-    text.innerHTML += "outerWidth : " +window.outerWidth +"<br>";
-    text.innerHTML += "outerHeight : " +window.outerHeight +"<br>";
-    // height.innerHTML = "innerHeight: " + window.innerHeight + "<br>";
+window.onscroll = function () {
+    text.innerHTML = "pageYOffset : " + window.pageYOffset + "<br>";
+    text.innerHTML += "pageXOffset : " + window.pageXOffset + "<br>";
 }
